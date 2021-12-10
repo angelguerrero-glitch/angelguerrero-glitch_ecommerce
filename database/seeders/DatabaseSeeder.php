1194a01814b40764eb02ec0f1271c6749298ef57
Angel J. Guerrero Gonzalez <angelguerrero@iesebre.com>
2021-12-10 16:11:24 +0100

    Creacio de llista finalitzat

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $product1 = Product::create([
+            'name'=> 'Producte 1',
+            'description'=> 'Camisa negra',
+            'image'=> 'http://lorempixel.com/400/200/food/1/',
+            'price'=> '50',
+        ]);
+        $product2 = Product::create([
+            'name'=> 'Producte 2',
+            'description'=> 'Camisa negra',
+            'image'=> 'http://lorempixel.com/400/200/food/2/',
+            'price'=> '30',
+        ]);
+        $product3 = Product::create([
+            'name'=> 'Producte 3',
+            'description'=> 'Camisa negra',
+            'image'=> 'http://lorempixel.com/400/200/food/3/',
+            'price'=> '40',
+        ]);
     }
 }
